3794389bd013c996627f124761b92e6188d42591
Daniel Clarke <d.clarke@queensu.ca>
2019-05-24 14:07:31 -0400

    Pushing assets

---
+++ Assets/ChangeRooms.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeRooms : MonoBehaviour {
+
+    float timer;
+
+    public bool end = false;
+
+    void Update() {
+        if (Input.GetAxis("openvr-r-triggerpull") > 0.1f || OVRInput.Get(OVRInput.Button.One))
+        {
+            timer += Time.deltaTime;
+            if (timer >= 2)
+            {
+                if (!end)
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                else
+                    SceneManager.LoadScene(0);
+            }
+        }
+        else
+            timer = 0;
+	}
+}
---
+++ Assets/Teleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public Transform black;
+    public Transform blue;
+    public Transform red;
+    public Transform green;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0)
+            transform.position = new Vector3(black.position.x, 4, black.position.z);
+        else if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0)
+            transform.position = new Vector3(blue.position.x, 4, blue.position.z);
+        else if(OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0)
+            transform.position = new Vector3(red.position.x, 4, red.position.z);
+        else if (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0)
+            transform.position = new Vector3(green.position.x, 4, green.position.z);
+    }
+}
